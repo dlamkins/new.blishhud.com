d9c8c5e5bd15042fde082822ace25690c8246b36
Dade Lamkins <dade@lamkins.org>
2023-01-31 12:37:38 -0500

    Added support for module sorting.

--- src/pages/modules.js
+++ src/pages/modules.js
@@ -7,9 +7,34 @@ import Head from '@docusaurus/Head';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
-function AllModules(modules) {
+/*
+
+    I apologize for my poor state handling in all of this.  React is clearly not something I'm super familiar with.
+    Please don't hesitate to recommend improvements.
+        — Freesnöw
+
+*/
+
+function AllModules(modules, [sortMethod, setSortMethod]) {
     const context = useDocusaurusContext();
     const { siteConfig = {} } = context;
+
+    context.updateSortOrder = (newSortOrder) => setSortMethod(newSortOrder.target.value);
+    
+    context.doSort = function (modules) {
+        switch (sortMethod) {
+            case "Downloads":
+                return modules.sort((a, b) => b.Downloads - a.Downloads);
+            case "A to Z":
+                return modules.sort((a, b) => a.Name.localeCompare(b.Name));
+            case "Z to A":
+                return modules.sort((a, b) => b.Name.localeCompare(a.Name));
+            case "Last Update":
+                return modules.sort((a, b) => new Date(b.LastUpdate) - new Date(a.LastUpdate));
+            default:
+                return modules;
+        }
+    }
 
     return (
         <Layout
@@ -18,25 +43,25 @@ function AllModules(modules) {
             <Head>
                 <meta name="keywords" content="Guild Wars 2, gw2, Blish, HUD, bhud, TacO, Overlay" />
             </Head>
-            <div class="module-content">
-                <div class="field has-addons" style={{ display: "none" }}>
-                    <p class="control">
-                        <a class="button is-static">
+            <div className="module-content">
+                <div className="field has-addons">
+                    <p className="control">
+                        <a className="button is-static">
                             Sort
                         </a>
                     </p>
-                    <div class="select is-right">
-                        <select>
+                    <div className="select is-right">
+                        <select id="sortOrder" value={context.sortMethod} onChange={context.updateSortOrder}>
+                            <option>Downloads</option>
                             <option>A to Z</option>
                             <option>Z to A</option>
-                            <option>Downloads</option>
                             <option>Last Update</option>
                         </select>
                     </div>
                 </div>
-                <div class="module-cards">
+                <div className="module-cards">
                     <BrowserOnly>{() =>
-                        modules && modules.map(module => (
+                        modules && context.doSort(modules).map(module => (
                                 <ModuleCard key={module.Namespace} module={module} />
                             ))
                         }
@@ -60,7 +85,7 @@ function Module(namespace, module) {
             </Head>
             <BrowserOnly>
                 {() => 
-                    <div class="module-content">
+                    <div className="module-content">
                         <ModuleProfile namespace={namespace} module={module} />
                     </div>
                 }
@@ -78,6 +103,7 @@ function Modules() {
     const [isLoaded, setIsLoaded] = useState(false);
     const [modules, setModules] = useState(null);
     const [module, setModule] = useState(null);
+    const [sortMethod, setSortMethod] = useState("Downloads");
 
     var urlParams = new URLSearchParams(window.location.search);
     var moduleNamespace = urlParams.get('module');
@@ -115,13 +141,13 @@ function Modules() {
     if (error) {
         return <label>Error loading: {error}</label>
     } else if (!isLoaded) {
-        return (<div class="hero">
+        return (<div className="hero">
             <h1>Loading...</h1>
         </div>)
     } else {
         if (moduleNamespace == null) {
             if (modules != null) {
-                return AllModules(modules);
+                return AllModules(modules, [sortMethod, setSortMethod]);
             } else {
                 setIsLoaded(false);
             }
