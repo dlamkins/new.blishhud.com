c348c1dbdf7e2bbc42c838a22db05f3c658f1390
Xander Lamkins <dade@lamkins.org>
2023-08-24 14:51:21 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,12 +108,12 @@ const config = {
                   </div>`,
       },
       announcementBar: {
-        id: 'apidown', // Any value that will identify this message.
+        id: 'discord', // Any value that will identify this message.
         content:
-          'The Guild Wars 2 API will be down <a href="https://en-forum.guildwars2.com/topic/135277-guild-wars-2-api-disabled-from-august-18-24/" target="_blank">until August 24th</a>. Until then, some Blish HUD features may exhibit unexpected behavior or otherwise not function. If you need assistance, please reach out in our <a target="_blank" href="https://discord.gg/FYKN3qh">Discord channel</a> for help.',
-        backgroundColor: '#7F0000', // Defaults to `#fff`.
+          'Check out our <a target="_blank" href="https://discord.gg/FYKN3qh">Discord channel</a> for the latest news, prerelease builds, and troubleshooting assistance.',
+        backgroundColor: '#8172DA', // Defaults to `#fff`.
         textColor: 'white', // Defaults to `#000`.
-        isCloseable: false, // Defaults to `true`.
+        isCloseable: true, // Defaults to `true`.
       },
       colorMode: {
         defaultMode: "dark",
