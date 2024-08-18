b330016e12b71da54b9baddac8af529fa4029f30
Xander Lamkins <dade@lamkins.org>
2024-08-18 13:59:14 -0400

    Fixed message format.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,7 +116,7 @@ const config = {
       announcementBar: {
         id: 'api', // Any value that will identify this message. (discord = default, api = api down)
         content:
-          'The Guild Wars 2 Web API is down until ArenaNet turns it back on sometime on August 23.  Some module features may not be available until the API returns.<br />Check out our <a target="_blank" href="https://discord.gg/FYKN3qh">Discord channel</a> for the latest news, prerelease builds, and troubleshooting assistance.',
+          'The Guild Wars 2 Web API is down <a target="_blank" href="https://en-forum.guildwars2.com/topic/149447-guild-wars-2-api-disabled-from-august-16-23/#comment-2164067">until ArenaNet turns it on August 23</a>. Some module features may not be available until the API returns. Join our <a target="_blank" href="https://discord.gg/FYKN3qh">Discord channel</a> for assistance.',
         backgroundColor: '#d94130', // Defaults to `#fff`.  We typically use: #8172DA for normal and #d94130 for warnings.
         textColor: 'white', // Defaults to `#000`.
         isCloseable: false, // Defaults to `true`.
